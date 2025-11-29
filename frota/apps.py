b89fe4f19dc814a0a31c1e9e2d58f9b276a3ed58
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-11-29 06:19:15 -0300

    chore: formata com ruff

--- frota/apps.py
+++ frota/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FrotaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frota'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "frota"
--- logistica/apps.py
+++ logistica/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LogisticaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'logistica'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "logistica"
