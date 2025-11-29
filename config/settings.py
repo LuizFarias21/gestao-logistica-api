471b7cea3981b9b403886d9d9adb74029ea71d0f
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-11-29 05:01:11 -0300

    chore: configura apps instalados em settings.py

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frota',
+    'logistica',
+    'usuarios',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
