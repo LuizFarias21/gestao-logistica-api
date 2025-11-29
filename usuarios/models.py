2b86b8be5d216365a057444ffc226721d21e9b2b
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-11-29 05:38:58 -0300

    feat: define modelo de usuário

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    TYPE_CHOICES = (
+        ('GESTOR', 'Gestor'),
+        ('MOTORISTA', 'Motorista'),
+        ('CLIENTE', 'Cliente'),
+    )
+    user_type = models.CharField(max_length=20, choices=TYPE_CHOICES, default='CLIENTE')
