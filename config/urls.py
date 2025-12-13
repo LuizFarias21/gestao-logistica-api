71cfa139720706b8f4c3c6a776cecfe93d200295
geysagraaziella0-eng <geysagraziella0@gmail.com>
2025-12-12 21:52:53 -0300

    #14 docs: configura Swagger e ReDoc

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    
     # Schema
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     # swagger UI
@@ -30,6 +31,13 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    # ReDoc
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    
     # apps
     path("api/usuarios/", include("usuarios.urls")),
     path("api/frota/", include("frota.urls")),
